91f37dda45929aa735ec57968e760963a1f91675
Erasmo <erasmoreginogonzalez@gmail.com>
2023-12-16 05:31:53 -0600

    Extract files to save storage

--- tictactoe-clone/src/App.jsx
+++ tictactoe-clone/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react"
 import confetti from "canvas-confetti"
-import './index.css'
 
 import { Square } from "./components/Square.jsx"
 import { TURNS } from "./constants.js"
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js"
 import { WinnerModal } from "./components/WinnerModal.jsx"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js"
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
-  
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+})
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return  turnFromStorage ?? TURNS.X
+  })
+
   //null es que no hay ganador, false hay un empate
   const [winner, setWinner] = useState(null)
 
@@ -20,6 +26,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -30,10 +38,14 @@ function App() {
     const newBoard = [... board]
     newBoard[index] = turn
     setBoard(newBoard)
-
     //cambiar el turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Guardar aqui Partida
+    saveGameToStorage({ 
+      board: newBoard,
+      turn: newTurn
+    })
     // Revisar si hay ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -46,7 +58,7 @@ function App() {
 
   return (
     <main className="board">
-      <h1>Tic tac toe</h1>
+      <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {
