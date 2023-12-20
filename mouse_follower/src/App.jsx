c3fa0dd6644b2d0abe2aea58f93e6bb51db6c748
Erasmo <erasmoreginogonzalez@gmail.com>
2023-12-20 15:27:08 -0600

    latest update, disable cursor

--- mouse_follower/src/App.jsx
+++ mouse_follower/src/App.jsx
@@ -4,6 +4,7 @@ const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0})
 
+  //pointer move
   useEffect(() => {
     console.log('effect', { enabled })
 
@@ -21,15 +22,24 @@ const FollowMouse = () => {
   // -> cuando el componente se desmonta
   // -> cuando cambian las dependencias, antes de ejecutar
   //    el efecto de nuevo
-  return () => {
+  return () => { //cleanup method
     console.log('cleanup')
     window.removeEventListener('pointermove', handleMove)
   }
-}, [enabled])
+  }, [enabled])
 
   // [] -> solo se ejecuta una vez cuando se monta el componente
   // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
   // undefined -> se ejecuta cada vez que se renderiza el componente
+
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  },  [enabled])
 
   return (
     <>
